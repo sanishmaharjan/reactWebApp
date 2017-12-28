45f6987a5f426e67ca7af53c54278aa6b0be20f3
SanishMaharjan <sanishmaharjan@lftechnology.com>
2017-12-28 16:22:38 +0545

    node server setup

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 8080));
+app.use(express.static(__dirname + '/app'));
+app.get('/', function(request, response) {
+    response.render('app/index.html');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
